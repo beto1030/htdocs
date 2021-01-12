37fa7a3935411eaf5a7575f64d085d41b370b14c
Alberto Alvarado <albertoalvarado@Albertos-Mac-mini.local>
2021-01-12 15:29:41 -0800

    first commit

---
+++ learning_databases/index.php
@@ -0,0 +1,20 @@
+            <?php
+                include_once 'header.php';
+            ?>
+            <main>
+                <section class="intro">
+                    <h1>This is an introduction</h1>
+                    <p>Here is an important paragraph that explains the purpose of the website</p>
+                </section>
+                <section class="categories">
+                    <h2>Basic Catagories</h2>
+                    <div class="items">
+                        <div>Item 1</div>
+                        <div>Item 2</div>
+                        <div>Item 3</div>
+                    </div>
+                </section>
+            </main>
+            <?php
+                include_once 'footer.php';
+            ?>
